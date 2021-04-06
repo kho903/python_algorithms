2b7ec484ac52756d555884412b8d56690cc07917
kho903 <gmldnr2222@naver.com>
2021-04-06 19:15:14 +0900

    2775

--- Baekjoon/08. 기본수학 1/2775.py
+++ Baekjoon/08. 기본수학 1/2775.py
@@ -1,4 +1,16 @@
 t = int(input())
-for i in range(t):
+a = []
+for _ in range(t):
     k = int(input())
     n = int(input())
+    a.append([k, n])
+res = []
+for i in range(t):
+    if a[i][0] == 0:
+        print(a[i][1])
+    else:
+        res = [i for i in range(1, a[i][1] + 1)]
+        for w in range(a[i][0]):
+            for q in range(1, len(res)):
+                res[q] = res[q] + res[q - 1]
+        print(res[-1])
