942a7e42afca5bc54966ee085f63fe0375ac76c5
kho903 <gmldnr2222@naver.com>
2021-04-28 17:37:39 +0900

    1946.py

---
+++ Baekjoon/알고리즘별/그리디/1946.py
@@ -0,0 +1,16 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    rank = []
+    for _ in range(n):
+        a, b = map(int, input().split())
+        rank.append((a, b))
+    rank.sort()
+    count = 1
+    max_r = rank[0][1]
+    for i in range(1, len(rank)):
+        if rank[i][1] < max_r:
+            count += 1
+            max_r = rank[i][1]
+    print(count)
