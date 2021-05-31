5d994ffb26b578ee8369758172fb8956f4c099a9
kho903 <gmldnr2222@naver.com>
2021-05-31 16:02:46 +0900

    10973.py

--- boj/브루트 포스/순열/10973.py
+++ boj/브루트 포스/순열/10973.py
@@ -1,11 +1,11 @@
 def prev_permutation(a):
     i = len(a) - 1
-    while i > 0 and a[i - 1] < a[i]:
+    while i > 0 and a[i - 1] <= a[i]:
         i -= 1
     if i <= 0:
         return False
     j = len(a) - 1
-    while a[j] > a[i - 1]:
+    while a[j] >= a[i - 1]:
         j -= 1
     a[i - 1], a[j] = a[j], a[i - 1]
     j = len(a) - 1
