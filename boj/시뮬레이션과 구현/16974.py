5a578a8b05977313a0d21ed5695f11b872cbb57f
kho903 <gmldnr2222@naver.com>
2021-06-26 17:36:28 +0900

    16974.py

---
+++ boj/시뮬레이션과 구현/16974.py
@@ -0,0 +1,30 @@
+n, x = map(int, input().split())
+d = [0] * (n + 1)
+p = [0] * (n + 1)
+d[0] = 1
+p[0] = 1
+
+for i in range(1, n + 1):
+    d[i] = 1 + d[i - 1] + 1 + d[i - 1] + 1
+    p[i] = p[i - 1] + 1 + p[i - 1]
+
+
+def solve(n, x):
+    if n == 0:
+        if x == 0:
+            return 0
+        else:
+            return 1
+    elif x == 1:
+        return 0
+    elif x <= 1 + d[n - 1]:
+        return solve(n - 1, x - 1)
+    elif x == 1 + d[n - 1] + 1:
+        return p[n - 1] + 1
+    elif x <= 1 + d[n - 1] + 1 + d[n - 1]:
+        return p[n - 1] + 1 + solve(n - 1, x - 1 - d[n - 1] - 1)
+    else:
+        return p[n - 1] + 1 + p[n - 1]
+
+
+print(solve(n, x))
