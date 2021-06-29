e0eb86b3233c0780d183f36ce249973fbfd19210
kho903 <gmldnr2222@naver.com>
2021-06-29 18:41:14 +0900

    내적.py

---
+++ programmers/level1/내적.py
@@ -0,0 +1,9 @@
+def solution(a, b):
+    ans = 0
+    for i in range(len(a)):
+        ans += a[i] * b[i]
+    return ans
+
+
+print(solution([1, 2, 3, 4], [-3, -1, 0, 2]))
+print(solution([-1, 0, 1], [1, 0, -1]))
