890350277cc709385912274f73d9d1508b0b2a35
kho903 <gmldnr2222@naver.com>
2021-08-13 15:17:11 +0900

    Data Structure / 04. 링크드리스트 - OOP_LinkedList

--- Data Structure/04. 링크드 리스트 (Linked List)/linked_list.py
+++ Data Structure/04. 링크드 리스트 (Linked List)/linked_list.py
@@ -29,3 +29,23 @@ while node.next:
     print(node.data)
     node = node.next
 print(node.data)
+
+# 링크드 리스트의 복잡한 기능1 (링크드 리스트 데이터 사이에 데이터를 추가)
+node3 = Node(1.5)
+node = head
+search = True
+while search:
+    if node.data == 1:
+        search = False
+    else:
+        node = node.next
+node_next = node.next
+node.next = node3
+node3.next = node_next
+
+print("=====================")
+while node.next:
+    print(node.data)
+    node = node.next
+print(node.data)
+
